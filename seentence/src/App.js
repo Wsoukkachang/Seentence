509b787fe717e3a9e352501225c098ec734e12a9
William Soukkachang <Wsoukkachang@yahoo.com>
2020-11-16 15:14:32 -0600

    Setup React app. Constructed wireframe and installed Material UI.

---
+++ seentence/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>This is search bar.</h1>
+      <h1>This is sentence word tabs.</h1>
+      <h1>This is image result container.</h1>
+      <h1>This is video result container.</h1>
+    </div>
+  );
+};
+
+export default App;
